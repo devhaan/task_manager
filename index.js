76585275a3e1c58ab2272a89dd287b5c0b641e97
devhaan <devemahore@gmail.com>
2024-12-04 20:53:56 +0530

    404 bad response

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(helmet());
 app.use(morgan("tiny"));
 
 app.use("/", taskManagementRouter);
+app.use((req, res, next) => {
+  res.status(404).send("404");
+});
 
 const port = process.env.PORT || 3000;
 
